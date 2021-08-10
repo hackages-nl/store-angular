49190fe5ca3795252169c876d126bafab8bc8c5c
Davy Engone <davy@hackages.io>
2021-08-10 15:02:47 +0200

    implemented filter by title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,26 @@
 import { Component } from '@angular/core';
 import { Book } from '../types/book';
 import { books as mockBooks } from '../mocks/books';
+import { BookService } from 'src/services/books.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Bookstore by Nalys';
 
+  // const bs = new BookService()
+  // this.bs = bs;
+  constructor(private bs: BookService) {
+    ///
+  }
+
   // Use mock data
   books: Book[] = mockBooks;
 
-  search() {
-    // Implement the search function
+  search(value: string) {
+    this.books = this.bs.search(value);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { BookService } from 'src/services/books.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [BookService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
